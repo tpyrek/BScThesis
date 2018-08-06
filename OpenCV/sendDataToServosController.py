b811cac454c7ff4909a17b85511dfa0bb62044b8
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-08-06 19:15:18 +0200

    Script to test Arduino-Python data sending - v2

--- OpenCV/sendDataToServosController.py
+++ OpenCV/sendDataToServosController.py
@@ -4,12 +4,18 @@ import struct
 import random
 
 
-order = "132456".encode()
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial()#('/dev/ttyUSB0', 9600)
+ser.port = '/dev/ttyUSB0'
+ser.baudrate = 9600
+ser.setDTR(True)
+ser.setRTS(True)
+ser.open()
 print(ser.readline())
 
-
 while True:
+
+
+
     s1 = random.randint(200, 800)
     s2 = random.randint(200, 800)
     s3 = random.randint(200, 800)
@@ -18,44 +24,12 @@ while True:
     s6 = random.randint(200, 800)
 
     a = struct.pack('<iiiiiiiiiBBBBBB', 0, 1000, 30, s1, s2, s3, s4, s5, s6,1,2,3,4,5,6)
-    print(s1)
-    print(s2)
-    print(s3)
-    print(s4)
-    print(s5)
-    print(s6)
-    print(order)
-
 
     ser.write(a)
 
     print("Read data:")
-    read = ser.readline()
-    read1 = ser.readline()
-    read2 = ser.readline()
-    read3 = ser.readline()
-    read4 = ser.readline()
-    read5 = ser.readline()
-    read6 = ser.readline()
-    read7 = ser.readline()
-    read8 = ser.readline()
-    read9 = ser.read()
-
-
-    for i in range(0,5):
-        read9 = read9 + (ser.read())
-
-    print(read)
-    print(read1)
-    print(read2)
-    print(read3)
-    print(read4)
-    print(read5)
-    print(read6)
-    print(read7)
-    print(read8)
-    print(read9)
-
     ready = ser.readline()
     print(ready)
+
+
 
