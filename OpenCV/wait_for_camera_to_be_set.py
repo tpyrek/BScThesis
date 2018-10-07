4a0e7b1b613ce4204a6047130f5ee3655dc4620a
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-10-07 13:39:03 +0200

    Function waiting for camera to be set properly - above all brightness

---
+++ OpenCV/wait_for_camera_to_be_set.py
@@ -0,0 +1,14 @@
+from PyQt5 import QtCore
+from time import sleep
+
+
+class waitForCameraToBeSet(QtCore.QObject):
+
+    sendCameraSet = QtCore.pyqtSignal()
+
+    def waitForCamera(self):
+        #Poczekać aż ustawi sie kamera (jasność,ostrość)
+
+        sleep(10)
+        self.sendCameraSet.emit()
+        return
