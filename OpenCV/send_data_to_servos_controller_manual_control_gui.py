741cba8be1f0f3b4237e786e9f1c3d5be96377e0
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-11-29 20:23:54 +0100

    renamed files and changed variables and functions names according to python code style for manual module

---
+++ OpenCV/send_data_to_servos_controller_manual_control_gui.py
@@ -0,0 +1,32 @@
+from PyQt5 import QtCore
+import struct
+
+
+# Do manualnej kontroli ramienia
+class SendDataToServosControllerManualControlGUI(QtCore.QObject):
+
+    # Signals
+    send_status = QtCore.pyqtSignal()
+    send_commands_text = QtCore.pyqtSignal(str)
+
+    def send_data(self, serial, min, max, speed, servo1_value, servo2_value, servo3_value, servo4_value,
+                  servo5_value, servo6_value, first_servo, second_servo, third_servo, fourth_servo, fifth_servo,
+                  sixth_servo):
+
+        if not serial.is_open:
+            self.send_commands_text.emit("Serial port jest zamknięty")
+            self.send_status.emit()
+            return
+
+        message_to_send = struct.pack('<iiiiiiiiiBBBBBB', min, max, speed, servo1_value, servo2_value, servo3_value,
+                                      servo4_value, servo5_value, servo6_value, first_servo, second_servo, third_servo,
+                                      fourth_servo, fifth_servo, sixth_servo)
+
+        serial.write(message_to_send)
+        self.send_commands_text.emit("Rpi   : " + str(message_to_send))
+
+        received_message = serial.readline()
+        self.send_commands_text.emit("Robot : " + str(received_message))
+
+        # Wysłanie sygnału o zakończonej pracy w celu oblokowania wigdetów
+        self.send_status.emit()
