f38d817ab300a01af55474d2a0fb2618029db4b6
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-11-29 12:19:09 +0100

    converted .ui files to python code with pyuic5 tool

--- OpenCV/instruction_gui.py
+++ OpenCV/instruction_gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'instruction.ui'
 #
-# Created by: PyQt5 UI code generator 5.11.2
+# Created by: PyQt5 UI code generator 5.11.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -17,13 +17,13 @@ class Ui_Dialog(object):
         self.gridLayout_2.setObjectName("gridLayout_2")
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.closePushButton = QtWidgets.QPushButton(Dialog)
-        self.closePushButton.setMinimumSize(QtCore.QSize(0, 30))
-        self.closePushButton.setObjectName("closePushButton")
-        self.gridLayout.addWidget(self.closePushButton, 1, 0, 1, 1)
-        self.textEdit = QtWidgets.QTextEdit(Dialog)
-        self.textEdit.setObjectName("textEdit")
-        self.gridLayout.addWidget(self.textEdit, 0, 0, 1, 1)
+        self.close_push_button = QtWidgets.QPushButton(Dialog)
+        self.close_push_button.setMinimumSize(QtCore.QSize(0, 30))
+        self.close_push_button.setObjectName("close_push_button")
+        self.gridLayout.addWidget(self.close_push_button, 1, 0, 1, 1)
+        self.text_edit = QtWidgets.QTextEdit(Dialog)
+        self.text_edit.setObjectName("text_edit")
+        self.gridLayout.addWidget(self.text_edit, 0, 0, 1, 1)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
 
         self.retranslateUi(Dialog)
@@ -32,8 +32,8 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Instrukcja"))
-        self.closePushButton.setText(_translate("Dialog", "Zamknij"))
-        self.textEdit.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        self.close_push_button.setText(_translate("Dialog", "Zamknij"))
+        self.text_edit.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
@@ -41,5 +41,5 @@ class Ui_Dialog(object):
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Instrukcja wykonania kalibracji:</p>\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">W celu dokonania kalibracji, ustawić figurę w danym kolorze pod kamerą tak, aby znacznik na podglądzie z kamery znajdował się na figurze. Następnie nacisnąć przycisk odpowiadający kolorowi figury.  Operacje powtórzyć dla wszystkich kolorów. Przycisk Ok zostanie odblokowany po kalibracji wszystkich kolorów.</p></body></html>"))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">W celu dokonania kalibracji, ustawić figurę w danym kolorze pod kamerą tak, aby znacznik na podglądzie z kamery znajdował się na figurze. Następnie nacisnąć przycisk odpowiadający kolorowi figury.  Operacje powtórzyć dla wszystkich kolorów.</p></body></html>"))
 
