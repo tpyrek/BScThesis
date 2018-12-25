cd658308c162f8d4b76d0f2468682668e20ef784
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-12-25 14:46:49 +0100

    fields coordinates order changed

--- OpenCV/auto_control/fields_coordinates.py
+++ OpenCV/auto_control/fields_coordinates.py
@@ -3,6 +3,6 @@ import numpy
 
 field_coordinates_table = {"field_1": numpy.array([655, 592, 260, 125, 70, 240], dtype=numpy.uint16),
                            "field_2": numpy.array([655, 592, 260, 125, 70, 240], dtype=numpy.uint16),
-                           "field_3": numpy.array([821, 431, 343, 296, 70, 240], dtype=numpy.uint16),
-                           "field_4": numpy.array([821, 551, 343, 296, 70, 240], dtype=numpy.uint16)}
+                           "field_3": numpy.array([821, 551, 343, 296, 70, 240], dtype=numpy.uint16),
+                           "field_4": numpy.array([821, 431, 343, 296, 70, 240], dtype=numpy.uint16)}
 
