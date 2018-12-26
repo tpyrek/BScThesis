5edd32cece226ec34aad1053cd492736d956d631
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-12-26 15:28:37 +0100

    set robot arm in the same position at each app closing

---
+++ OpenCV/send_data_to_servos/send_data_to_servos_controller_at_app_closing.py
@@ -0,0 +1,19 @@
+from PyQt5 import QtCore
+import struct
+
+
+# Do ustawienia ramienia po zakończeniu działania aplikacji
+class SendDataToServosControllerAtAppClosing(QtCore.QObject):
+
+    def send_data(self, serial):
+
+        if not serial.is_open:
+            self.send_commands_text.emit("Serial port jest zamknięty")
+            self.send_status.emit()
+            return
+
+        message_to_send = struct.pack('<iiiiiiiiiBBBBBB', 0, 1000, 30, 473, 132, 73, 86, 70, 230, 5, 6, 2, 4, 3, 1)
+
+        serial.write(message_to_send)
+
+        serial.readline()
