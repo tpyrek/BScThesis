03e6a9b6515055b9bab8e08b4d203a5c11a80ec3
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-12-29 16:09:06 +0100

    deleted unnecessary lines

--- OpenCV/send_data_to_servos/send_data_to_servos_controller_at_app_closing.py
+++ OpenCV/send_data_to_servos/send_data_to_servos_controller_at_app_closing.py
@@ -7,15 +7,10 @@ class SendDataToServosControllerAtAppClosing(QtCore.QObject):
 
     def send_data(self, serial):
 
-        print('ok')
         if not serial.is_open:
-            self.send_commands_text.emit("Serial port jest zamknięty")
-            self.send_status.emit()
             return
 
         message_to_send = struct.pack('<iiiiiiiiiBBBBBB', 0, 1000, 30, 473, 132, 73, 86, 70, 230, 5, 6, 2, 4, 3, 1)
-
         serial.write(message_to_send)
-
         serial.readline()
 
