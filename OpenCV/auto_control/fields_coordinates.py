e6710591e3c4b5c77694376e3cd14399a8cc941a
Tomasz Pyrek <tompyrek@student.agh.edu.pl>
2018-12-30 18:59:24 +0100

    fields coordinates servos values changed

--- OpenCV/auto_control/fields_coordinates.py
+++ OpenCV/auto_control/fields_coordinates.py
@@ -1,8 +1,8 @@
 import numpy
 
 
-field_coordinates_table = {"field_1": numpy.array([571, 397, 234, 117, 80, 200], dtype=numpy.uint16),
-                           "field_2": numpy.array([571, 571, 234, 117, 80, 200], dtype=numpy.uint16),
-                           "field_3": numpy.array([750, 543, 356, 242, 80, 210], dtype=numpy.uint16),
-                           "field_4": numpy.array([750, 430, 356, 242, 70, 210], dtype=numpy.uint16)}
+field_coordinates_table = {"field_1": numpy.array([571, 397, 234, 117, 70, 200], dtype=numpy.uint16),
+                           "field_2": numpy.array([571, 571, 234, 117, 70, 200], dtype=numpy.uint16),
+                           "field_3": numpy.array([750, 543, 356, 242, 70, 200], dtype=numpy.uint16),
+                           "field_4": numpy.array([750, 430, 356, 242, 70, 200], dtype=numpy.uint16)}
 
